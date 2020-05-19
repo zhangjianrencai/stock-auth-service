3aba7f3265601f603abf41fafb17b6cbc8237498
zhangjianrencai <1025962108@qq.com>
2020-05-19 17:44:08 +0800

    update

--- src/main/java/com/stock/StockServiceAuthApplication.java
+++ src/main/java/com/stock/StockServiceAuthApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class StockServiceApplication {
+public class StockServiceAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockServiceApplication.class, args);
+		SpringApplication.run(StockServiceAuthApplication.class, args);
 	}
 
 }
