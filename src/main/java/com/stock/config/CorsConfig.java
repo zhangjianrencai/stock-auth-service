eb580fe96974dbd4bd2bc7db70677c92ca2eb98d
zhangjianrencai <1025962108@qq.com>
2020-06-05 17:02:44 +0800

    update configure

--- src/main/java/com/stock/config/CorsConfig.java
+++ src/main/java/com/stock/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
    public CorsFilter corsFilter() {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
-      config.setAllowCredentials(true);
+      config.setAllowCredentials(false);
       config.addAllowedOrigin("*"); // e.g. http://domain1.com
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
